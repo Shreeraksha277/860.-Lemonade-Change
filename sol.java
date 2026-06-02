791e1b284f5995fd9d0db141c27699381b1279fc
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-06-02 17:23:50 +0530

    Create sol.java

---
+++ sol.java
@@ -0,0 +1,47 @@
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        int twenty=0;
+        for(int i=0;i<bills.length;i++)
+        {
+            if(bills[i]==5)
+            {
+                five++;
+            }
+            else if(bills[i]==10)
+            {
+                if(five>=1)
+                {
+                    five--;
+                    ten++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if(bills[i]==20)
+            {
+                if(five>=1 && ten>=1)
+                {
+                    ten--;
+                    five--;
+                    twenty++;
+                }
+                else if(five>=3)
+                {
+
+                five-=3;
+                twenty++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    return true;
+    }
+}

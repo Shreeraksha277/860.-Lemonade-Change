f45d99a33b49d32cba8c04df9a45aa6227f67fb5
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-06-04 20:53:00 +0530

    Update sol.java

--- sol.java
+++ sol.java
@@ -1,7 +1,7 @@
 
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int five=0;
+         int five=0;
         int ten=0;
         int twenty=0;
         for(int i=0;i<bills.length;i++)
